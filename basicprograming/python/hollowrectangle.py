2f438063a2ba3f8149c63d511bace98c4c391e2d
Shraddha Tripathi <tripathishraddha191@gmail.com>
2024-09-08 13:11:27 +0530

    rectangle pattern

---
+++ basicprograming/python/hollowrectangle.py
@@ -0,0 +1,9 @@
+row, col = map(int, input().split())
+
+for i in range(1, row + 1):
+    for j in range(1, col + 1):
+        if i == 1 or i == row or j == 1 or j == col:
+            print('*', end='')
+        else:
+            print(' ', end='')
+    print()
